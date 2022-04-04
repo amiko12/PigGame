0a4edda389e11c98f3532ffe657cbfb791e2ea0d
Amiko <amgeladze10@gmail.com>
2022-04-04 20:59:32 +0300

    ups

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ var array = [
 ];
 
 var player1CN = document.getElementById("player1CN");
-var player2CN = document.getElementById("player2CN");   
+var player2CN = document.getElementById("player2CN");
 var player1 = document.getElementById("player1");
-var player2 = document.getElementById("player2")
+var player2 = document.getElementById("player2");
 var start = document.getElementById("start");
 start.addEventListener("click", () => {
   document.getElementById("hold").style.display = "block";
@@ -41,16 +41,11 @@ var block = document.getElementById("x");
 function roll() {
   var random = Math.floor(Math.random() * 6);
   img.src = array[random].image;
-  
   block.appendChild(img);
   img.style.display = "block";
   /////////////////////////////////////////////////////
-
-  /////////////////////////////////////////////////////
-  
   if (random == 0) {
     player2.classList.toggle("active");
-    player1.classList.remove("active1");
     player1CN.innerText = 0;
     player2CN.innerText = 0;
   } else {
@@ -58,93 +53,61 @@ function roll() {
     player1.classList.add("avtive1");
   }
   /////////////////////////////////////////////////////////////////
-
-  var active = player2.className
+  var active = player2.className;
   if (random > 0) {
-    
-      if( active == 'active'){
-        var counter = random + 1;
-        var z = parseInt(player2CN.textContent);
-        var sum1 = z + counter;
-        player2CN.innerText = sum1;
-        player1CN.innerText = 0;
-      }else if (active !== 'active'){
-        var current = random + 1;
-        var y = parseInt(player1CN.textContent);
-        var sum = y + current;
-        player1CN.innerText = sum;
-        player2CN.innerText = 0;       
-      }
-
-  }; 
-  console.log(random);
-
-};
-
+    if (active == "active") {
+      var counter = random + 1;
+      var z = parseInt(player2CN.textContent);
+      var sum1 = z + counter;
+      player2CN.innerText = sum1;
+      player1CN.innerText = 0;
+    } else if (active !== "active") {
+      var current = random + 1;
+      var y = parseInt(player1CN.textContent);
+      var sum = y + current;
+      player1CN.innerText = sum;
+      player2CN.innerText = 0;
+    }
+  }
+}
 ////////////////////////////////////////
-var newgamebtn = document.getElementById('winnerbtn');
-var btndiv = document.getElementById('btndiv');
+var newgamebtn = document.getElementById("winnerbtn");
+var btndiv = document.getElementById("btndiv");
 var score = document.getElementById("realscore");
-var inscore = document.getElementById('realscore2');
-var winner = document.getElementById('winnerh1');
-document.getElementById('hold').addEventListener('click', ()=>{
-
-    // player 1
-    var x = parseInt(player1CN.textContent);
-    
-    var s = parseInt(score.textContent);
-    var realscore = s + x;
-    score.innerText = realscore;
-    player1CN.innerText = 0;
-    player2.classList.toggle("active");
-
-    // player 2
-    var holdscore = parseInt(player2CN.textContent);
-    
-    var inner = parseInt(inscore.textContent);
-    var text = inner + holdscore;
-    inscore.innerText = text;
-    player2CN.innerText = 0;   
-    
-    
-    // endGame
-    if(score.textContent >= 100 || inscore.textContent >= 100){
-        btndiv.style.display = 'none';
-        
-        newgamebtn.style.display = 'block';
-
-        
-        if(score.textContent >= 100){
-          
-          winner.innerText = "PLAYER 1 has won the game";
-            
-        }
-        else if (inscore.textContent >= 100){
-            winner.innerText = 'PLAYER 2 has won the game';
-            
-        };
-        
-    };
+var inscore = document.getElementById("realscore2");
+var winner = document.getElementById("winnerh1");
+document.getElementById("hold").addEventListener("click", () => {
+  // player 1
+  var x = parseInt(player1CN.textContent);
+  var s = parseInt(score.textContent);
+  var realscore = s + x;
+  score.innerText = realscore;
+  player1CN.innerText = 0;
+  player2.classList.toggle("active");
+  // player 2
+  var holdscore = parseInt(player2CN.textContent);
+  var inner = parseInt(inscore.textContent);
+  var text = inner + holdscore;
+  inscore.innerText = text;
+  player2CN.innerText = 0;
+  // endGame
+  if (score.textContent >= 100 || inscore.textContent >= 100) {
+    btndiv.style.display = "none";
+    newgamebtn.style.display = "block";
+    if (score.textContent >= 100) {
+      winner.innerText = "PLAYER 1 has won the game";
+    } else if (inscore.textContent >= 100) {
+      winner.innerText = "PLAYER 2 has won the game";
+    }
+  }
 });
-
-
-function newgame(){
-  
-
+function newgame() {
   score.innerText = 0;
   inscore.innerText = 0;
-
-  winner.style.display = 'none';
-  newgamebtn.style.display = 'none';
-
-  btndiv.style.display = 'flex';
-
-};
-
-
-
-
-
+  winner.style.display = "none";
+  newgamebtn.style.display = "none";
+  btndiv.style.display = "flex";
+}
 
 /* function hold1(){
     var x = parseInt(player1CN.textContent);
